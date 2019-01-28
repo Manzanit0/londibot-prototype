c9b41fcf74d205e959d4991e78167d6594555748
Manzanit0 <unico.javier@gmail.com>
2019-01-28 12:41:05 +0000

    :bug: Fixes responses to properly display for everyone in the channel.

--- project.clj
+++ project.clj
@@ -9,7 +9,11 @@
                  [morse                     "0.2.4"] ; Telegram API specific
                  [clj-http                  "3.9.1"]
                  [compojure                 "1.6.1"]
+                 [ring/ring-core            "1.7.1"]
                  [ring/ring-jetty-adapter   "1.7.1"]
+                 [ring-logger               "1.0.1"]
+                 [ring-server               "0.5.0"]
+                 [org.clojure/data.json     "0.2.6"]
                  [org.immutant/scheduling   "2.1.10"]
                  [org.clojure/java.jdbc     "0.7.8"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]]
@@ -28,3 +32,4 @@
                                 :database-uri "jdbc:postgresql://localhost:5432/londibot_test"}}
              :production {:env {:clj-env :production
                                 :database-uri "jdbc:postgresql://localhost:5432/londibot"}}})
+
--- src/londibot/slack/bot.clj
+++ src/londibot/slack/bot.clj
@@ -1,7 +1,9 @@
 (ns londibot.slack.bot
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.adapter.jetty :refer :all]
+            [ring.logger :refer :all]
+            [ring.server.standalone :refer :all]
+            [clojure.data.json :as json]
             [environ.core :refer [env]]
             [londibot.core.messages :refer :all]
             [londibot.core.tfl :refer :all])
@@ -9,21 +11,30 @@
 
 (def connection {:api-url "https://slack.com/api" :token (env :slack-token)})
 
-; FIXME - Configure logger in order to see if the header is set correctly.
-(defn in-channel-visibility [handler]
-  ; Makes the message visible to all the channel members.
+(defn wrap-with-header [handler k v]
   (fn [request]
     (let [response (handler request)]
-      (assoc-in response [:headers "response_type"] "in_channel"))))
+      (assoc-in response [:headers k] v))))
+
+(defn print-response-stdout [handler]
+  (fn [request]
+    (let [response (handler request)]
+      (println response) response)))
+
+(defn generate-response [body]
+  ; Generates responses visible to everyone in the channel by default.
+  (json/write-str {:text body :response_type "in_channel"}))
 
 (defroutes main-routes
-  (POST "/tube-status" [] (tube-status-message (tube-status)))
+  (POST "/tube-status" [] (generate-response (tube-status-message (tube-status))))
   (route/not-found "Page not found"))
 
 (def app
   (-> main-routes
-      (in-channel-visibility)))
+      (wrap-with-header "Content-Type" "application/json")
+      (wrap-with-logger)
+      (print-response-stdout)))
 
 (defn -main
   [& args]
-  (run-jetty app {:port 5000}))
+  (serve app {:port 5000 :open-browser? false}))
